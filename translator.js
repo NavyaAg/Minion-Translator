7dfb3f16cb2bb7f305f1e0f4ddae25369fb38140
NavyaAg <itsNavya001@gmail.com>
2021-01-20 14:48:57 +0530

    translator api added

--- translator.js
+++ translator.js
@@ -1,11 +1,21 @@
-var translate=document.querySelector("button");
-var inputText=document.querySelector("textarea");
-var outputText=document.querySelector("#outputText");
-translate.addEventListener("click",function(){
-
+var translate = document.querySelector("button");
+var inputText = document.querySelector("textarea");
+var outputText = document.querySelector("#outputText");
+translate.addEventListener("click", function() {
+  var translated = doFetch(inputText.value);
   console.log(inputText.value);
-  console.log(outputText.innerText,"i am navya");
+  // outputText.innerText = (translated);
 })
 
 // var inputText=document.body.querySelector("textarea");
 // console.log(inputText);
+
+function urlGenerator(text) {
+  return ("		https://api.funtranslations.com/translate/minion.json" + "?text=" + text);
+}
+
+function doFetch(Text) {
+  fetch(urlGenerator(Text))
+    .then(response => response.json())
+    .then(json =>  outputText.innerText= (json.contents.translated))
+}
